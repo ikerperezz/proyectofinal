15865b71b1e33b0038d9c0229de9aa4a4d158b6d
Josu Igoa <josu.igoa@opendeusto.es>
2023-01-03 20:33:47 +0100

    Implementadas las funcionalidades de la VentanaAñadirAdmin

--- proyectoFInal/src/ventanas/VentanaAñadirAdmin.java
+++ proyectoFInal/src/ventanas/VentanaAñadirAdmin.java
@@ -5,17 +5,31 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import clases.Administrador;
+
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
 import java.awt.Font;
 import javax.swing.SwingConstants;
 import javax.swing.JTextField;
 import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.awt.event.ActionEvent;
+import javax.swing.JPasswordField;
+import java.awt.Color;
 
 public class VentanaAñadirAdmin extends JFrame {
 
 	private JPanel contentPane;
 	private JTextField textField;
-	private JTextField textField_2;
+	private JPasswordField passwordField;
 
 	/**
 	 * Launch the application.
@@ -66,21 +80,92 @@ public class VentanaAñadirAdmin extends JFrame {
 		contentPane.add(textField);
 		textField.setColumns(10);
 		
-		textField_2 = new JTextField();
-		textField_2.setColumns(10);
-		textField_2.setBounds(223, 160, 168, 30);
-		contentPane.add(textField_2);
-		
 		JButton btnNewButton = new JButton("CREAR ADMINISTRADOR");
-		btnNewButton.setBounds(249, 223, 177, 41);
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				if (textField.getText().trim().isEmpty()
+						|| String.valueOf(passwordField.getText()).trim().isEmpty()) {
+
+					JOptionPane.showMessageDialog(VentanaAñadirAdmin.this,
+							"Hay campos obligatorios vacios, rellene todos");
+					textField.setText("");
+					passwordField.setText("");
+					
+				if (!textField.getText().trim().isEmpty()
+							&& (!String.valueOf(passwordField.getText()).trim().isEmpty())){
+					
+					String nombreUsuario=textField.getText();
+					String contraseña=String.valueOf(passwordField.getText());
+					
+					
+					String adminNuevo=String.format("%s, %s", nombreUsuario, contraseña);
+							
+					
+					try (BufferedWriter writer = new BufferedWriter(new FileWriter("src/ficheroExterno/Administradores.csv"))) {
+						
+						writer.write(adminNuevo);
+						
+						writer.close();
+												
+					} catch (IOException e1) {
+		                System.out.println("No se pudo escribir en la ruta indicada o no se encontro el fichero");
+		            }
+					
+					
+					JOptionPane.showMessageDialog(VentanaAñadirAdmin.this,
+							"Administrador añadido con exito");
+					
+					textField.setText("");
+					passwordField.setText("");
+					
+				}
+				
+					
+				}
+				
+			}
+		});
+		btnNewButton.setBounds(249, 234, 177, 41);
 		contentPane.add(btnNewButton);
 		
 		JButton btnBorrar = new JButton("BORRAR");
-		btnBorrar.setBounds(31, 223, 177, 41);
+		btnBorrar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				textField.setText("");
+				passwordField.setText("");
+				
+			}
+		});
+		btnBorrar.setBounds(31, 234, 177, 41);
 		contentPane.add(btnBorrar);
 		
 		JButton btnVolver = new JButton("Volver");
+		btnVolver.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				VentanaAdministrador v = new VentanaAdministrador();
+				v.setVisible(true);
+				VentanaAñadirAdmin.this.setVisible(false);
+				
+			}
+		});
 		btnVolver.setBounds(158, 286, 120, 29);
 		contentPane.add(btnVolver);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(223, 155, 168, 30);
+		contentPane.add(passwordField);
+		
+		JLabel lblNewLabel_1 = new JLabel("*Este campo es obligatorio");
+		lblNewLabel_1.setForeground(new Color(255, 0, 0));
+		lblNewLabel_1.setBounds(229, 123, 152, 14);
+		contentPane.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_1_1 = new JLabel("*Este campo es obligatorio");
+		lblNewLabel_1_1.setForeground(Color.RED);
+		lblNewLabel_1_1.setBounds(229, 196, 152, 14);
+		contentPane.add(lblNewLabel_1_1);
 	}
 }
