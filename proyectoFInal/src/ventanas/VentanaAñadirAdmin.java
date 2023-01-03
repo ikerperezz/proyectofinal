5032d8cc4a95bbc19977196626bf8c4bae73036b
Josu Igoa <josu.igoa@opendeusto.es>
2023-01-03 19:45:02 +0100

    Implemenado diseño de VentanaAñadirAdmin, faltan las funcionalidades

--- proyectoFInal/src/ventanas/VentanaAñadirAdmin.java
+++ proyectoFInal/src/ventanas/VentanaAñadirAdmin.java
@@ -5,10 +5,17 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.JTextField;
+import javax.swing.JButton;
 
 public class VentanaAñadirAdmin extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_2;
 
 	/**
 	 * Launch the application.
@@ -31,11 +38,49 @@ public class VentanaAñadirAdmin extends JFrame {
 	 */
 	public VentanaAñadirAdmin() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 450, 352);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("DATOS DEL NUEVO ADMINISTRADOR");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblNewLabel.setBounds(31, 11, 350, 50);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNombreDeUsuario = new JLabel("Nombre de usuario");
+		lblNombreDeUsuario.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblNombreDeUsuario.setBounds(31, 72, 192, 50);
+		contentPane.add(lblNombreDeUsuario);
+		
+		JLabel lblContrasea = new JLabel("Contraseña");
+		lblContrasea.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblContrasea.setBounds(31, 148, 192, 50);
+		contentPane.add(lblContrasea);
+		
+		textField = new JTextField();
+		textField.setBounds(223, 81, 168, 30);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		textField_2 = new JTextField();
+		textField_2.setColumns(10);
+		textField_2.setBounds(223, 160, 168, 30);
+		contentPane.add(textField_2);
+		
+		JButton btnNewButton = new JButton("CREAR ADMINISTRADOR");
+		btnNewButton.setBounds(249, 223, 177, 41);
+		contentPane.add(btnNewButton);
+		
+		JButton btnBorrar = new JButton("BORRAR");
+		btnBorrar.setBounds(31, 223, 177, 41);
+		contentPane.add(btnBorrar);
+		
+		JButton btnVolver = new JButton("Volver");
+		btnVolver.setBounds(158, 286, 120, 29);
+		contentPane.add(btnVolver);
 	}
-
 }
