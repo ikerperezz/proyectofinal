3b7ae031ba4ab1d561d52d4a30bbee368525a3f2
IkerGlz <iker.gonrek@opendeusto.es>
2023-01-08 23:32:26 +0100

    Borrado el contenido de la ventana Ventana Hilo

--- proyectoFInal/src/ventanas/VentanaHilo.java
+++ proyectoFInal/src/ventanas/VentanaHilo.java
@@ -27,57 +27,15 @@ public class VentanaHilo extends JFrame {
 	 */
 	private static final long serialVersionUID = 1L;
 
-	public VentanaHilo(VentanaMercado ventanamercado) {
+	public VentanaHilo(VentanaAdministrador ventanaAdministrador) {
 
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 474, 214);
+		setBounds(100, 100, 493, 363);
 		JPanel contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-
-		JButton btnNewButton = new JButton("Ver jugadores mas valorados");
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				Thread hilo = new Thread() {
-					@Override
-					public void run() {
-						VentanaHilo2 v = new VentanaHilo2();
-						v.setVisible(true);
-						DBManager dbmanager = new DBManager();
-						dbmanager.conectar();
-						Connection conexion = dbmanager.getConn();
-						try (PreparedStatement stmt = conexion.prepareStatement(
-								"SELECT nombreJugador, valor FROM jugadores ORDER BY valor DESC LIMIT 5 ")) {
-							ResultSet resultSet = stmt.executeQuery();
-							DefaultListModel<String> model = new DefaultListModel<>();
-							while (resultSet.next()) {
-								model.addElement(resultSet.getString("nombreJugador"));
-								model.addElement(resultSet.getString("valor"));
-							}
-							JList<String> listaJugadores = new JList<>(model);
-
-						} catch (SQLException e) {
-							e.printStackTrace();
-						}
-					}
-				};
-				hilo.start();
-				Logger logger = Logger.getLogger("Final de evento");
-				logger.info("Evento finalizado");
-			}
-		});
-		getContentPane().add(btnNewButton, BorderLayout.WEST);
-
-		JButton btnNewButton_1 = new JButton("Volver");
-		btnNewButton_1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				VentanaMercado v = new VentanaMercado();
-				v.setVisible(true);
-				VentanaHilo.this.setVisible(false);
-			}
-		});
 
 	}
 }
