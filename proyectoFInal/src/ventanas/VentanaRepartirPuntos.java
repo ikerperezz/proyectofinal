3fa63c438b02867463965cd3e79a2e7d30e796dd
Josu Igoa <josu.igoa@opendeusto.es>
2023-01-09 20:35:15 +0100

    Metodo cargar jlist implementado

--- proyectoFInal/src/ventanas/VentanaRepartirPuntos.java
+++ proyectoFInal/src/ventanas/VentanaRepartirPuntos.java
@@ -4,18 +4,28 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.border.EmptyBorder;
+
+import baseDatos.DBManager;
+
 import javax.swing.JList;
 import javax.swing.JLabel;
 import java.awt.Font;
 import javax.swing.JSpinner;
+import javax.swing.SpinnerModel;
+import javax.swing.SpinnerNumberModel;
+import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.util.List;
+import java.util.Map;
 import java.awt.event.ActionEvent;
 
 public class VentanaRepartirPuntos extends JFrame {
 
 	private JPanel contentPane;
+	private DefaultListModel<String> model;
 
 	/**
 	 * Launch the application.
@@ -45,9 +55,13 @@ public class VentanaRepartirPuntos extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JList list = new JList();
-		list.setBounds(33, 98, 172, 237);
-		contentPane.add(list);
+			
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(33, 98, 172, 237);
+		contentPane.add(scrollPane);
+		cargarJList();
+		JList<String> list = new JList<String>(model);
+		scrollPane.setViewportView(list);
 		
 		JLabel lblNewLabel = new JLabel("USUARIOS");
 		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 14));
@@ -59,11 +73,20 @@ public class VentanaRepartirPuntos extends JFrame {
 		lblPuntos.setBounds(352, 139, 74, 14);
 		contentPane.add(lblPuntos);
 		
-		JSpinner spinner = new JSpinner();
+		SpinnerModel spModel_1 = new SpinnerNumberModel(0, //initial value
+                0, //min
+                9, //max
+                1); //step
+		SpinnerModel spModel = new SpinnerNumberModel(0, //initial value
+                0, //min
+                9, //max
+                1); //step
+		
+		JSpinner spinner = new JSpinner(spModel);
 		spinner.setBounds(312, 138, 30, 20);
 		contentPane.add(spinner);
 		
-		JSpinner haundi = new JSpinner();
+		JSpinner haundi = new JSpinner(spModel_1);
 		haundi.setBounds(272, 138, 30, 20);
 		contentPane.add(haundi);
 		
@@ -77,5 +100,39 @@ public class VentanaRepartirPuntos extends JFrame {
 		});
 		btnNewButton.setBounds(287, 194, 105, 36);
 		contentPane.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("volver");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				VentanaAdministrador v= new VentanaAdministrador();
+				v.setVisible(true);
+				VentanaRepartirPuntos.this.setVisible(false);
+				
+			}
+		});
+		btnNewButton_1.setBounds(272, 312, 89, 23);
+		contentPane.add(btnNewButton_1);
 	}
+	
+	public void cargarJList() {
+		// TODO Auto-generated method stub
+		DBManager dbmanager= new DBManager();
+		dbmanager.conectar();
+		Map<String, Integer> usuario = dbmanager.crearListaUsuariosPuntos();
+		
+		List<String> nom = dbmanager.crearListaUsuariosNombre();
+		
+		model = new DefaultListModel<String>();
+		
+	for (int i = 0; i < nom.size(); i++) {
+		int value = usuario.get(nom.get(i));
+		String key = nom.get(i);
+		
+		model.addElement(key +" - "+ value + "puntos");
+	}
+			
+			dbmanager.disconnect();
+			
+		}
 }
